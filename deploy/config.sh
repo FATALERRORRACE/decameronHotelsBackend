97436722941c8d9fb958cbf96e5c87b109be7141
FATALERRORRACE <frdrummer@gmail.com>
2025-01-28 11:35:49 -0500

    paso a paso

--- deploy/config.sh
+++ deploy/config.sh
@@ -1,86 +1,59 @@
-sudo yum install -y httpd php php-gd php-xml php-mbstring php-json php-zip php-pgsql php-pdo_pgsql git php8.1-fpm
-sudo mkdir -p /var/www/ && sudo chown -R www-data:www-data /var/www/
-sudo git clone https://github.com/FATALERRORRACE/decameronHotelsBackend.git /var/www/
-sudo wget https://getcomposer.org/composer.phar
-sudo chmod +x composer.phar
-sudo mv composer.phar /usr/local/bin/composer
-cd /var/www/ 
-sudo composer install
-sudo yum install -y https://download.postgresql.org/pub/repos/yum/reporpms/EL-7-x86_64/pgdg-redhat-repo-latest.noarch.rpm --skip-broken
-sudo yum install -y postgresql15 postgresql15-server postgresql15-contrib
-sudo postgresql-setup --initdb
-sudo systemctl start postgresql
-sudo systemctl enable postgresql
-sudo passwd postgres
-Admin123
-Admin123
-sudo systemctl start postgresql
-su - postgres
-sudo -i -u postgres psql
-psql -c "ALTER USER postgres WITH PASSWORD 'your-password';"
-#execute this command doesnt require login with postgres user
-sudo -i -u postgres psql
-CREATE USER admin WITH PASSWORD 'Admin123';
-CREATE DATABASE decameron;
-GRANT ALL PRIVILEGES ON DATABASE decameron TO admin;
-# INSTALL NGINX
-sudo dnf install nginx
-sudo systemctl enable nginx.service
-sudo systemctl restart nginx.service
+## Instalación 
+   ##  1. INSTALAR PAQUETES:
+        sudo yum install -y httpd php php-gd php-xml php-mbstring php-json php-zip php-pgsql php-pdo_pgsql git php8.1-fpm
+        sudo yum install -y postgresql15 postgresql15-server postgresql15-contrib
+        sudo dnf install nodejs
+        sudo dnf install nginx
 
-sudo cp /var/www/decameronHotelsBackend/deploy/decameron-backend.conf /etc/nginx/conf.d/decameron-backend.conf
+    ## 2. DESCARGAR REPOSITORIOS
+        sudo mkdir /var/www/
+        sudo git clone https://github.com/FATALERRORRACE/decameronHotelsBackend.git /var/www/
+        sudo chown -R nginx:nginx /var/www/decameronHotelsBackend
+        sudo git clone https://github.com/FATALERRORRACE/decameronHotelsFrontend.git /var/www/
+        sudo chown -R nginx:nginx /var/www/decameronHotelsBackend
+    
+    ## 3. CREAR BASE DE DATOS:
+       ##  activar servicios:
+            sudo postgresql-setup --initdb
+            sudo systemctl start postgresql
+            sudo systemctl enable postgresql
+            sudo passwd postgres // para agregar contraseña su posgres
 
-sudo cp /var/www/decameronHotelsBackend/deploy/decameron-backend.conf /usr/share/nginx/modules/decameron-backend.conf
+        ## crear: 
+            sudo -i -u postgreUser psql
+            "CREATE DATABASE decameron;"
+            permisos: 
+            CREATE USER admin WITH PASSWORD 'Admin123';
+            CREATE DATABASE decameron;
+            GRANT ALL PRIVILEGES ON DATABASE decameron TO admin;
 
+    ## 4. INSTALAR SERVIDOR (NGINX): 
+        sudo systemctl enable nginx.service
+        sudo cp /var/www/decameronHotelsBackend/deploy/decameron-backend.conf /etc/nginx/conf.d/decameron-backend.conf
+        sudo cp /var/www/decameronHotelsBackend/deploy/decameron-frontend.conf /usr/share/nginx/modules/decameron-frontend.conf
+        sudo systemctl restart nginx.service
 
+    ## 5. INSTANCIAR SERVIDOR BACKEND:
+        ## instalar composer:
+            sudo wget https://getcomposer.org/composer.phar
+            sudo chmod +x composer.phar
+            sudo mv composer.phar /usr/local/bin/composer
+            cd /var/www/decameronHotelsBackend
+            sudo composer install
+        ## agregar tablas:
+            sudo php artisan migrate
+        ## agregar registros:
+            sudo php artisan db:seed
+        ## optimizar en cache contenido:
+            sudo php artisan optimize
 
+    ## 6. INSTANCIAR SERVIDOR FRONTEND:
+        cd /var/www/decameronHotelsFrontend
+        sudo npm i 
+        sudo npm run build
+        sudo npm i pm2 -g 
+        sudo pm2 start "npm run start -- -p 8080" --name decafront
 
-
-
-
-
-
-
-
-
-sudo apt install php-cli php-mbstring php-xml php-bcmath php-curl unzip
-curl -sS https://getcomposer.org/installer | php
-sudo mv composer.phar /usr/local/bin/composer
-sudo apt install nginx
-# Install repo
-sudo mkdir -p /var/www/ && sudo chown -R www-data:www-data /var/www/
-git clone https://github.com/FATALERRORRACE/decameronHotelsBackend.git /var/www/
-cp /var/www/decameronHotelsBackend
-sudo chown -R www-data:www-data /var/www/decameronHotelsBackend
-sudo chmod -R 775 /var/www/decameronHotelsBackend/storage /var/www/decameronHotelsBackend/bootstrap/cache
-cd /var/www/decameronHotelsBackend
-composer install --no-dev --optimize-autoloader
-
-
-sudo ln -s /etc/nginx/sites-available/decameron-backend.conf /etc/nginx/conf.d/
-sudo systemctl restart nginx
-# Install postgre
-sudo apt install -y postgresql-common
-sudo /usr/share/postgresql-common/pgdg/apt.postgresql.org.sh
-
-
-# Set variables
-DB_USER="admin"
-DB_PASSWORD="Admin123"
-DB_ROLE="READWRITE"
-DB_HOST="localhost"
-DB_PORT="5432"
-
-# Execute SQL commands
-sudo -u postgres psql -c "CREATE USER admin WITH PASSWORD 'Admin123';"
-sudo -u postgres psql -c "ALTER USER admin WITH READWRITE;"
-sudo -u postgres psql -c "CREATE DATABASE decameron;"
-
-echo "User admin created with role "READWRITE"."
-
-#laravel config
-php artisan migrate
-php artisan db:seed
-php artisan config:cache
-php artisan route:cache
-
+    ## 7. CONFIGURAR HOSTS:
+        54.158.42.57   decameron-api-pablo.co
+        54.158.42.57   decameron-pablo.co
